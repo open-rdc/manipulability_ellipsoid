50e4ee31c2a60e740ea64656e7a4be643a65ab9a
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2019-02-16 12:11:45 +0900

    velocity

--- bezier.m
+++ bezier.m
@@ -8,6 +8,13 @@ function [x, y] = bezier(P, t)
   y = X(2);
 endfunction
 
+function [xd, yd] = bezier_d(P, t)
+  Bd =[-3*(1-t)^2, 3*(1-t)^2-6*t*(1-t), 6*t*(1-t)-3*t^2, 3*t^2];
+  Xd = Bd * P;
+  xd = Xd(1);
+  yd = Xd(2);
+endfunction
+
 %i = 0
 %P = [0, 0; 0, 0.06; 0.1+0.07, 0.06; 0.1, 0.0]
 %ttt = 0.0:0.1:1.0
--- manipulability_bezier.m
+++ manipulability_bezier.m
@@ -10,18 +10,34 @@ function [t1, t2] = inverse_kinematics(x, y, l1, l2)
   endif
 endfunction
 
+function [x, y] = bezier(P, t)
+  B =[(1-t)^3, 3*t*(1-t)^2, 3*t^2*(1-t), t^3];
+  X = B * P;
+  x = X(1);
+  y = X(2);
+endfunction
+
+function [xd, yd] = bezier_d(P, t)
+  Bd =[-3*(1-t)^2, 3*(1-t)^2-6*t*(1-t), 6*t*(1-t)-3*t^2, 3*t^2];
+  Xd = Bd * P;
+  xd = Xd(1);
+  yd = Xd(2);
+endfunction
+
 l1 = 0.11;
 l2 = 0.11;
 ratio = 0.1;
 motor_velocity = 4.81; # 46rpm
 
 P = [0, 0; 0, 0.06; 0.1+0.07, 0.06; 0.1, 0.0];
+period = 0.32;
 
 i = 0;
 for t = 0.0: 0.2: 1.0
   [xt, yt] = bezier(P, t);
   x = xt - 0.05;
   y = yt - 0.16;
+
   [t1, t2] = inverse_kinematics(x, y, l1, l2);
   if (t1 != 0 || t2 != 0)
     i = i + 1;
@@ -32,6 +48,9 @@ for t = 0.0: 0.2: 1.0
     jaco = [-l1*sin(t1)-l2*sin(t1+t2) -l2*sin(t1+t2); l1*cos(t1)+l2*cos(t1+t2) l2*cos(t1+t2)];
     [U,S,V] = svd(jaco);
     mani(i,:) = [x y atan2(U(2),U(1)) S(1,1) S(2,2)];
+	[xd, yd] = bezier_d(P, t);
+    xdp(i,:) = [x x + xd / period / motor_velocity * ratio];
+    ydp(i,:) = [y y + yd / period / motor_velocity * ratio];
   endif
 endfor
 
@@ -40,6 +59,7 @@ axis([-0.2,0.2,-0.2,0.2],"square");
 for n = 1:i
   plot(arm_x(n,:), arm_y(n,:));
   ellipse(mani(n,1), mani(n,2), mani(n,4)*ratio, mani(n,5)*ratio, mani(n,3));
+  plot(xdp(n,:), ydp(n,:), "r");
   if (mani(n,3) < 0)
     mani(n,3) += 2 * pi;
   endif

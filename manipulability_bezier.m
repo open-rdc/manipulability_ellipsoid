f2b1e071bf6997c61adf3b1de98812efcf2a3673
yasuohayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2019-02-22 22:59:06 +0900

    new trajectory

--- manipulability_bezier.m
+++ manipulability_bezier.m
@@ -24,19 +24,19 @@ function [xd, yd] = bezier_d(P, t)
   yd = Xd(2);
 endfunction
 
-l1 = 0.11;
-l2 = 0.11;
+l1 = 0.108;
+l2 = 0.108;
 ratio = 0.1;
 motor_velocity = 4.81; # 46rpm
 
-P = [0, 0; 0, 0.06; 0.1+0.07, 0.06; 0.1, 0.0];
+P = [0, 0; 0, 0.1; 0.3, 0.1; 0.2, 0.0];
 period = 0.32;
 
 i = 0;
 for t = 0.0: 0.2: 1.0
   [xt, yt] = bezier(P, t);
-  x = xt - 0.05;
-  y = yt - 0.16;
+  x = xt - 0.1;
+  y = yt - 0.18;
 
   [t1, t2] = inverse_kinematics(x, y, l1, l2);
   if (t1 != 0 || t2 != 0)
@@ -45,7 +45,7 @@ for t = 0.0: 0.2: 1.0
     y1 = l1 * sin(t1);
     arm_x(i,:) = [0 x1 x];
     arm_y(i,:) = [0 y1 y];
-    jaco = [-l1*sin(t1)-l2*sin(t1+t2) -l2*sin(t1+t2); l1*cos(t1)+l2*cos(t1+t2) l2*cos(t1+t2)];
+    jaco = [-l1*sin(t1) -l2*sin(t2); l1*cos(t1) l2*cos(t2)];
     [U,S,V] = svd(jaco);
     mani(i,:) = [x y atan2(U(2),U(1)) S(1,1) S(2,2)];
 	[xd, yd] = bezier_d(P, t);
@@ -55,7 +55,7 @@ for t = 0.0: 0.2: 1.0
 endfor
 
 hold on;
-axis([-0.2,0.2,-0.2,0.2],"square");
+axis([-0.3,0.3,-0.3,0.3],"square");
 for n = 1:i
   plot(arm_x(n,:), arm_y(n,:));
   ellipse(mani(n,1), mani(n,2), mani(n,4)*ratio, mani(n,5)*ratio, mani(n,3));

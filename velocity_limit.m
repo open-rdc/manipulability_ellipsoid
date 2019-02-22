f1a80a0d123a5b33212e6cbc96df276102197782
yasuohayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2019-02-23 00:37:00 +0900

    add velocity limit

--- velocity_limit.m
+++ velocity_limit.m
@@ -29,17 +29,24 @@ l2 = 0.108;
 ratio = 0.1;
 motor_velocity = 4.81; # 46rpm
 
-P = [0, 0; 0, 0.06; 0.17, 0.06; 0.1, 0.0];
+P = [0, 0; 0, 0.1; 0.3, 0.1; 0.2, 0.0];
 period = 0.30;
-v = [-1 -1 1 1 -1; -1 1 1 -1 -1];
+
+xr = -0.10;
+yr = -0.18;
+[t1r, t2r] = inverse_kinematics(xr, yr, l1, l2);
 
 i = 0;
-for t = 0.0: 0.2: 1.0
+dt = 0.01;
+for t = 0.0: dt/period: period/period
   [xt, yt] = bezier(P, t);
-  x = xt - 0.05;
+  x = xt - 0.10;
   y = yt - 0.18;
 
   [t1, t2] = inverse_kinematics(x, y, l1, l2);
+  t1r = t1r + max([min([t1 - t1r, motor_velocity * dt]), -motor_velocity * dt]);
+  t2r = t2r + max([min([t2 - t2r, motor_velocity * dt]), -motor_velocity * dt]);
+
   if (t1 != 0 || t2 != 0)
     i = i + 1;
     x1 = l1 * cos(t1);
@@ -48,22 +55,28 @@ for t = 0.0: 0.2: 1.0
     y2 = l1 * sin(t2) + y1;
     arm_x(i,:) = [0 x1 x2];
     arm_y(i,:) = [0 y1 y2];
-    jaco = [-l1*sin(t1) -l2*sin(t2); l1*cos(t1) l2*cos(t2)];
-	m = jaco * v;
-	mx(i,:) = m(1,:) * ratio + x;
-	my(i,:) = m(2,:) * ratio + y;
-	[xd, yd] = bezier_d(P, t);
-    xdp(i,:) = [x x + xd / period / motor_velocity * ratio];
-    ydp(i,:) = [y y + yd / period / motor_velocity * ratio];
+    xdp(i) = x2;
+    ydp(i) = y2;
+    x1r = l1 * cos(t1r);
+    y1r = l1 * sin(t1r);
+    x2r = l2 * cos(t2r) + x1r;
+    y2r = l2 * sin(t2r) + y1r;
+    arm_xr(i,:) = [0 x1r x2r];
+    arm_yr(i,:) = [0 y1r y2r];
+    xdpr(i) = x2r;
+    ydpr(i) = y2r;
   endif
 endfor
 
 hold on;
 axis([-0.15,0.15,-0.25,0.05],"square");
 for n = 1:i
-  plot(arm_x(n,:), arm_y(n,:));
-  plot(xdp(n,:), ydp(n,:), "r");
-  plot(mx(n,:), my(n,:));
+  if (mod(n - 1, 5) == 0)
+    plot(arm_x(n,:), arm_y(n,:));
+    plot(arm_xr(n,:), arm_yr(n,:), "r");
+  endif
+  plot(xdp, ydp);
+  plot(xdpr, ydpr, "r");
 end
 hold off;
 
